ba0cc7fa09ca4686125ab9387c09c71d3a6c5089
Rafał Krupiński <rkrupinski+github@gmail.com>
2016-11-21 22:37:18 +0000

    Fix blocking, add Wait to block until SIGINT | SIGTERM

--- http-poll.go
+++ http-poll.go
@@ -4,9 +4,28 @@ import (
 	"fmt"
 	"github.com/rafalkrupinski/http-poll/tasks"
 	"time"
+	"os/signal"
+	"os"
+	"syscall"
 )
 
+func StartMulti(allTasks []*tasks.TaskState) {
+	for _, task := range allTasks {
+		go doLoop(task)
+	}
+}
+
 func Start(task *tasks.TaskState) {
+	go doLoop(task)
+}
+
+func Wait() {
+	exitSignal := make(chan os.Signal)
+	signal.Notify(exitSignal, syscall.SIGINT, syscall.SIGTERM)
+	<-exitSignal
+}
+
+func doLoop(task *tasks.TaskState) {
 	ticker := time.NewTicker(task.Frequency)
 	go doRun(task)
 	for range ticker.C {
